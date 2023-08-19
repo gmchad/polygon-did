ddd0b32f7eb5318f4316797e18b964e6a1a294b6
MetacrafterChris <125202934+MetacrafterChris@users.noreply.github.com>
2023-08-19 22:26:35 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,12 +5,19 @@ dotenv.config()
 const network: string = "testnet";
 
 async function main() {
+  // pad private key with 0x
+  let pk = process.env.PRIVATE_KEY;
+  if (!pk.startsWith("0x")) {
+    // pad 0x to private key
+    pk = `0x${pk}`;
+  }
+
   // create a new did
-  const did = await createDID(network, process.env.PRIVATE_KEY);
+  const did = await createDID(network, pk);
   console.log("DID created: ", did.data.did);
 
   // register the did
-  const txHash = await registerDID(did.data.did, process.env.PRIVATE_KEY);
+  const txHash = await registerDID(did.data.did, pk);
   console.log("DID registered: ", txHash);
 }
 
